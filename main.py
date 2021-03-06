5be0a75861798f418fd9cd056e62067b774b13b9
gresse <gresse@gressePC>
2021-03-06 23:28:01 +0100

    Different layers are now composed

--- main.py
+++ main.py
@@ -11,96 +11,94 @@ from PIL import ImageFilter
 
 width = 1920
 height = 1080
-layers = 1
 
 
-class Canvas:
+class Graph:
 
-    def __init__(self):
+    def __init__(self, graph_style, center, min_rad, max_rad, line_width, color_style, blur_rad):
         global width, height
+
+        # Get variables
+        self.graph_style = graph_style
+        self.center = center
+        self.min_rad = min_rad
+        self.max_rad = max_rad
+        self.line_width = line_width
+        self.color_style = color_style
+        self.blur_rad = blur_rad
 
         # Init the image and drawer
         self.image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
         self.draw = ImageDraw.Draw(self.image)
 
-        """
-        self.color_map = color_map((255, 0, 0, 100),
-                                   (0, 255, 0, 255),
-                                   (0, 0, 255, 255),
-                                   (255, 0, 0, 200))
-        """
+        # Setup color
+        if self.color_style == 'c':
+            # Choose a color if color style is a constant color
+            self.color = rand_color()
+        elif self.color_style == 'a':
+            # Choose a full band angle if color style is angle and the alpha value
+            self.full_band_angle = random.randrange(15, 200) / 10
+            self.color_alpha = random.randrange(200, 255)
+        elif self.color_style == 'm':
+            # Create/Choose a color map if the style is a map
+            self.color_map = color_map(rand_color(), rand_color(), rand_color(), rand_color())
+        else:
+            print('Wrong Color style')
 
-    def draw_epicycloid(self, max_rad, min_rad, center, line_width):
+        # Draw the graph
+        if self.graph_style == 'et':
+            self.draw_epitrochoid()
+        elif self.graph_style == 'ht':
+            self.draw_hypotrochoid()
+        else:
+            print("Wroung Graph style")
 
-        # Start angle
-        teta = 0
+        # Blur the graph
+        self.image = self.image.filter(ImageFilter.GaussianBlur(self.blur_rad))
 
-        # Outer circle
-        r = 0
-
-        # Factor for the relation between outer and inner circle
-        k = 0
-
-        # Search for for one set until to outer circle of the epicycloid is in the dimensions
-        while (2 * r + k * r) < min_rad or (2 * r + k * r) > max_rad:
-            r = random.randrange(int(max_rad * 0.05), int(max_rad * 0.6))
-            k = random.randrange(5, 50) / 10
-
-        # Get start pos for teta = 0
-        start_x = int(round(center[0] + r * (k + 1) * math.cos(teta) - r * math.cos((k + 1) * teta)))
-        start_y = int(round(center[1] + r * (k + 1) * math.sin(teta) - r * math.sin((k + 1) * teta)))
-
-        while True:
-
-            # Calc pos
-            x = int(round(center[0] + r * (k + 1) * math.cos(teta) - r * math.cos((k + 1) * teta)))
-            y = int(round(center[1] + r * (k + 1) * math.sin(teta) - r * math.sin((k + 1) * teta)))
-
-            # Draw
-            rad = line_width / 2
+    def get_draw_color(self, x, y, teta):
+        # Setup color
+        if self.color_style == 'c':
+            return self.color
+        elif self.color_style == 'a':
+            return tuple(color_angle(teta, self.full_band_angle, self.color_alpha))
+        elif self.color_style == 'm':
             try:
-                color = tuple(self.color_map[y][x])
+                return tuple(self.color_map[y][x])
             except:
-                color = (0, 0, 0, 255)
-            self.draw.ellipse((x - rad, y - rad, x + rad, y + rad), fill=color)
+                return tuple((0, 0, 0, 0))
 
-            # Check if at beginning of circle and check that teta is at least 2 pi
-            if start_x == x and start_y == y and teta >= 2 * math.pi:
-                break
-            else:
-                # Advance teta
-                teta += 0.002
+        else:
+            return tuple((0, 0, 0, 0))
 
-    def draw_hypocycloid(self, max_rad, min_rad, center, line_width):
+    def draw_epitrochoid(self):
 
-        # Start angle
-        teta = 0
-
-        # rotating circle
-        r = 0
-
-        # Factor for the relation between rotating and static circle
-        k = 0
+        teta = random.randrange(0, 62) / 10  # Start angle
+        r = 0  # rotating circle
+        k = 0  # Factor for the relation between rotating and static circle
+        d = 0  # distance between the draw point and the center of the rotating circle
 
         # Search for for one set until to outer circle of the epicycloid is in the dimensions
-        while (k * r) < min_rad or (k * r) > max_rad:
-            r = random.randrange(int(max_rad * 0.05), int(max_rad * 0.2))
-            k = random.randrange(5, 50) / 10
-            # k = random.randrange(5, 10) / random.randrange(5, 15)
+        while (k * r + r + d) < self.min_rad or (k * r + r + d) > self.max_rad:
+            r = random.randrange(int(self.max_rad * 0.05), int(self.max_rad * 0.6))
+            k = random.randrange(25, 50) / 10
+            d = r * random.randrange(5, 30) / 10
+
+        print("Epitrochoid: r = {0} \tk = {1} \td/r = {2}".format(r, k, d / r))
 
         # Get start pos for teta = 0
-        start_x = int(round(center[0] + r * (k - 1) * math.cos(teta) + r * math.cos((k - 1) * teta)))
-        start_y = int(round(center[1] + r * (k - 1) * math.sin(teta) - r * math.sin((k - 1) * teta)))
+        start_x = int(round(self.center[0] + r * (k + 1) * math.cos(teta) - d * math.cos((k + 1) * teta)))
+        start_y = int(round(self.center[1] + r * (k + 1) * math.sin(teta) - d * math.sin((k + 1) * teta)))
 
         while True:
 
             # Calc pos
-            x = int(round(center[0] + r * (k - 1) * math.cos(teta) + r * math.cos((k - 1) * teta)))
-            y = int(round(center[1] + r * (k - 1) * math.sin(teta) - r * math.sin((k - 1) * teta)))
+            x = int(round(self.center[0] + r * (k + 1) * math.cos(teta) - d * math.cos((k + 1) * teta)))
+            y = int(round(self.center[1] + r * (k + 1) * math.sin(teta) - d * math.sin((k + 1) * teta)))
 
             # Draw
-            rad = line_width / 2
-            self.draw.ellipse((x - rad, y - rad, x + rad, y + rad), fill=color_angle(teta, 60, 255))
+            rad = self.line_width / 2
+            self.draw.ellipse((x - rad, y - rad, x + rad, y + rad), fill=self.get_draw_color(x, y, teta))
 
             # Check if at beginning of circle and check that teta is at least 2 pi
             if start_x == x and start_y == y and teta >= 2 * math.pi:
@@ -109,35 +107,35 @@ class Canvas:
                 # Advance teta
                 teta += 0.002
 
-    def draw_hypotrochoid(self, max_rad, min_rad, center, line_width):
+    def draw_hypotrochoid(self):
 
-        teta = 0  # Start angle
+        teta = random.randrange(0, 62) / 10  # Start angle
         r = 0  # rotating circle
         k = 0  # Factor for the relation between rotating and static circle
         d = 0  # distance between the draw point and the center of the rotating circle
 
         # Search for for one set until to outer circle of the epicycloid is in the dimensions
 
-        while (k * r - r + d) < min_rad or (k * r - r + d) > max_rad:
-            r = random.randrange(int(max_rad * 0.05), int(max_rad * 0.15))
+        while (k * r - r + d) < self.min_rad or (k * r - r + d) > self.max_rad:
+            r = random.randrange(int(self.max_rad * 0.05), int(self.max_rad * 0.15))
             k = random.randrange(5, 50) / 10
-            d = r * random.randrange(5, 50)/10
+            d = r * random.randrange(5, 50) / 10
 
-        print("Hypotrochoid: r = {0} \tk = {1} \td/r = {2}".format(r, k, d/r))
+        print("Hypotrochoid: r = {0} \tk = {1} \td/r = {2}".format(r, k, d / r))
 
         # Get start pos for teta = 0
-        start_x = int(round(center[0] + r * (k - 1) * math.cos(teta) + d * math.cos((k - 1) * teta)))
-        start_y = int(round(center[1] + r * (k - 1) * math.sin(teta) - d * math.sin((k - 1) * teta)))
+        start_x = int(round(self.center[0] + r * (k - 1) * math.cos(teta) + d * math.cos((k - 1) * teta)))
+        start_y = int(round(self.center[1] + r * (k - 1) * math.sin(teta) - d * math.sin((k - 1) * teta)))
 
         while True:
 
             # Calc pos
-            x = int(round(center[0] + r * (k - 1) * math.cos(teta) + d * math.cos((k - 1) * teta)))
-            y = int(round(center[1] + r * (k - 1) * math.sin(teta) - d * math.sin((k - 1) * teta)))
+            x = int(round(self.center[0] + r * (k - 1) * math.cos(teta) + d * math.cos((k - 1) * teta)))
+            y = int(round(self.center[1] + r * (k - 1) * math.sin(teta) - d * math.sin((k - 1) * teta)))
 
             # Draw
-            rad = line_width / 2
-            self.draw.ellipse((x - rad, y - rad, x + rad, y + rad), fill=(0, 0, 0, 255))
+            rad = self.line_width / 2
+            self.draw.ellipse((x - rad, y - rad, x + rad, y + rad), fill=self.get_draw_color(x, y, teta))
 
             # Check if at beginning of circle and check that teta is at least 2 pi
             if start_x == x and start_y == y and teta >= 2 * math.pi:
@@ -147,12 +145,28 @@ class Canvas:
                 teta += 0.002
 
 
+def rand_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    # Make it transparent only with a small chance
+    trans = random.randrange(0, 3)
+    if trans == 0:
+        a = random.randint(100, 255)
+    else:
+        a = 255
+
+    ret = (r, g, b, a)
+    return ret
+
+
 def color_angle(angle, full_band_angle, alpha):
     """
     One color rgb value is always 0
-    :param alpha: alpha value for the color, stays constant
     :param full_band_angle: at what angle the color band starts over
     :param angle: angle for which the color
+    :param alpha: alpha value for the color, stays constant
     :return: color
     """
 
@@ -229,16 +243,99 @@ def compose_layers(images):
     return out
 
 
-def main():
-    global width, height, layers
+def create_layer(layer_style):
+    """
+    In this function the basic random stuff is handled,
+    like the position, the size, the blur,
+    if the color should be from a map from the angle or const
 
-    c = Canvas()
-    c.draw_hypotrochoid(height/2 + 300, height/2 - 300, (width/2, height/2), 5)
-    c.image.show()
+    The fine tuning of the graph and what color specifically is handled in the
+    canvas object itself
 
-    # c.image = c.image.filter(ImageFilter.GaussianBlur(radius=20))
-    # c.image.show()
+    :param layer_style: b - background; m - middle; f - foreground
+    :return: image of the layer
+    """
+    global width, height
+
+    # Set the random layer parameter stuff
+    if layer_style == 'b':
+        graph_style = random.choice(['et', 'ht'])
+        center = [random.randrange(int(width * 0.1), int(width * 0.9)),
+                  random.randrange(int(height * 0.1), int(height * 0.9))]
+        min_rad = height
+        max_rad = width
+        line_width = random.randrange(12, 18)
+        color_style = random.choice(['c', 'c', 'a', 'm', 'm'])
+        blur_rad = random.randrange(8, 20)
+
+    elif layer_style == 'm':
+        graph_style = random.choice(['et', 'ht'])
+        center = [random.randrange(int(width * 0.2), int(width * 0.8)),
+                  random.randrange(int(height * 0.2), int(height * 0.8))]
+        min_rad = height / 2
+        max_rad = width / 2
+        line_width = random.randrange(8, 12)
+        color_style = random.choice(['c', 'a', 'm'])
+        blur_rad = random.randrange(6, 12)
+
+    elif layer_style == 'f':
+        graph_style = random.choice(['et', 'ht'])
+        center = [random.randrange(int(width * 0.3), int(width * 0.7)),
+                  random.randrange(int(height * 0.3), int(height * 0.7))]
+        min_rad = height * 0.4
+        max_rad = width * 0.6
+        line_width = random.randrange(3, 5)
+        color_style = random.choice(['c', 'c', 'm'])
+        blur_rad = random.randrange(1, 2)
+
+    else:
+        print("Wrong style")
+        return None
+
+    # Draw layer
+    layer = Graph(graph_style, center, min_rad, max_rad, line_width, color_style, blur_rad)
+
+    return layer.image
+
+
+def generate_image():
+    global width, height
+
+    # Set number of layers
+    b_layers = random.randrange(1, 3)
+    m_layers = random.randrange(1, 3)
+    f_layers = random.randrange(1, 3)
+
+    # Background
+    layers = [Image.new('RGBA', (width, height), (0, 0, 0, 255))]
+
+    # Create background layers
+    for i in range(b_layers):
+        new_layer = create_layer('b')
+        layers.append(new_layer)
+
+    # Create middle layers
+    for i in range(m_layers):
+        new_layer = create_layer('m')
+        layers.append(new_layer)
+
+    # Create foreground layers
+    for i in range(f_layers):
+        new_layer = create_layer('f')
+        layers.append(new_layer)
+
+    # Combine layers
+    composed_image = compose_layers(layers)
+
+    return composed_image
 
 
 if __name__ == "__main__":
-    main()
+    i = 0
+    while True:
+        print()
+        img = generate_image()
+        img.save('images/img_{0}.png'.format(str(i).rjust(3, '0')))
+        print('Image generated')
+
+        i += 1
